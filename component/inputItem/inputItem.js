d682cde037259ddf60ada657ebfa54435534709a
hepengfei <13022835919@163.com>
2018-10-11 22:50:36 +0800

    学校编辑页面组件完成

---
+++ component/inputItem/inputItem.js
@@ -0,0 +1,36 @@
+// component/inputItem/inputItem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    diabled:{
+      type:Boolean,
+      value:true
+    },
+    right:{
+      type:Boolean,
+      value:true
+    },
+    spac:{
+      type:Number,
+      value:8
+    },
+    title:String,
+    value:String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
