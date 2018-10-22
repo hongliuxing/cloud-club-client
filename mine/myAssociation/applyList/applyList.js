54e5eb6344f573f8303ab59a0c2f33ae0c4bc229
kobe <13811421514@sina.cn>
2018-10-22 11:58:43 +0800

    申请列表修改

--- mine/myAssociation/applyList/applyList.js
+++ mine/myAssociation/applyList/applyList.js
@@ -9,9 +9,24 @@ Page({
    */
   data: {
     topBtns: [],
-    club_id:"",
-    struts:0,
-    pagenum:1,
+    club_id: "",
+    struts: 0,
+    pagenum: 1,
+
+    AuditingList: [], //审核相关
+    AuditingRefersh: true,
+    AuditingPagenum: 1,
+
+    passList: [], //已通过
+    passRefersh: true,
+    passPagenum: 1,
+
+    on_passList: [], //未通过通过
+    on_passRefersh: true,
+    on_passPagenum: 1,
+   
+    btn:true,//判断是否显示button
+
   },
 
   /**
@@ -39,64 +54,201 @@ Page({
       topBtns: topBtns
     });
     let item = wx.getStorageSync("associationInfo");
-    if (item){
-       let data = {
-         club_id:item.id,
-         struts:that.data.struts, 
-         pagenum :that.data.pagenum
-       }
-       that._request(data)
+    if (item) {
+        that.setData({
+          club_id: item.id,
+        })
+      let data = {
+        club_id: item.id,
+        struts: that.data.struts,
+        pagenum: that.data.pagenum
+      }
+      wx.showLoading({ title: '正在加载...', mask: true });
+      that._request(data)
     }
 
   },
 
   //审核中
-  Auditing(){
+  Auditing() {
+    that.setData({
+      struts: 0,
+      btn:true
+    })
 
+    if(this.data.AuditingList.length==0){
+
+      that._request({ struts: 0, pagenum:1})
+
+    }else{
+      return
+    }
   },
-  
+
   //已通过
-  pass(){
+  pass() {
+    that.setData({
+      struts: 1,
+      btn: false
+    })
+    if (this.data.passList.length == 0) {
 
+      that._request({ struts: 1, pagenum: 1 })
+
+    } else {
+      return
+    }
   },
-
   //未通过
-  no_pass(){
+  no_pass() {
+    that.setData({
+      struts: -1,
+      btn: false
+    })
+    if (this.data.on_passList.length == 0) {
+
+      that._request({ struts: -1, pagenum: 1 })
+
+    } else {
+      return
+    }
+  },
+  //批准
+  onAgree(e){
+    wx.showModal({
+      title: '提示',
+      content: '',
+    })
+    return
+    let id = e.currentTarget.dataset.applyId;
+    let apply_client_id = e.currentTarget.dataset.applyClientId;
+    let name = e.currentTarget.dataset.name;
+    Actions.doPost({
+      ulr: URLs.CLUBMASTER_JOIN_RATIFY,
+      data:{
+        apply_id:id,
+        club_id: that.data.club_id,
+        apply_client_id: apply_client_id
+      }
+    }).then(res=>{
+      
+    }).catch(error=>{
+
+    })
 
   },
+  //拒绝
+  onRefuse(e){
+    let id = e.currentTarget.dataset.applyId;
+    let apply_client_id = e.currentTarget.dataset.applyClientId;
+    Actions.doPost({
+      ulr: URLs.CLUBMASTER_JOIN_REJECT,
+      data: {
+        apply_id: id,
+        club_id: that.data.club_id,
+        apply_client_id: apply_client_id
+      }
+    }).then(res => {
 
-  _request({ club_id = that.data.club_id, struts = that.data.struts, pagenum = that.data.pagenum}){
-   Actions.doGet({
-     url: URLs.CLUBMASTER_JOIN_LIST,
-     data:{
-       club_id: club_id,
-       struts : struts,
-       pagenum : pagenum
-     }
-   }).then(res=>{
-     console.log(res.data.list[0]["crole.profe"],"uuuuuuuuuuuuuuuu")
-   }).catch(error=>{
+    }).catch(error => {
 
-   })
+    })
+  },
+  //请求
+  _request({
+    club_id = that.data.club_id,
+    struts = that.data.struts,
+    pagenum = that.data.pagenum
+  }) {
+    let arr;
+    if (struts == 0) {
+      arr = pagenum == 1 ? [] : that.data.AuditingList;
+    } else if (struts == 1){
+      arr = pagenum == 1 ? [] : that.data.passList;
+    }else{
+      arr = pagenum == 1 ? [] : that.data.on_passList;
+    }
+
+    Actions.doGet({
+      url: URLs.CLUBMASTER_JOIN_LIST,
+      data: {
+        club_id: club_id,
+        struts: struts,
+        pagenum: pagenum
+      }
+    }).then(res => {
+      wx.hideLoading()
+      arr = arr.concat(res.data.list)
+      if (res.data.list.length>0){
+        if (struts == 0) {
+          that.setData({
+            AuditingRefersh: true,
+            AuditingList:arr,
+            AuditingPagenum: pagenum
+          })
+        } else if (struts == 1) {
+          that.setData({
+            passRefersh: true,
+            passList:arr,
+            passPagenum: pagenum
+          })
+        } else {
+          that.setData({
+            on_passRefersh: true,
+            on_passList:arr,
+            on_passPagenum: pagenum
+          })
+        }
+      }else{
+        if (struts == 0) {
+          that.setData({
+            AuditingRefersh: false,
+            AuditingPagenum: pagenum
+          })
+        } else if (struts == 1) {
+          that.setData({
+            passRefersh: false,
+            passPagenum: pagenum
+          })
+        } else {
+          that.setData({
+            on_passRefersh: false,
+            on_passPagenum: pagenum
+          })
+        }
+      }
+      wx.stopPullDownRefresh()
+    }).catch(error => {
+
+    })
   },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    that._request({ pagenum: 1 })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-
+    if (that.data.struts == 0) {
+      if (that.data.AuditingRefersh){
+        that._request({ pagenum: that.data.AuditingPagenum+1 })
+        wx.showLoading({ title: '加载更多...', mask: true });
+        }
+    } else if (struts == 1) {
+      if (that.data.passRefersh) {
+        that._request({ pagenum: that.data.passPagenum + 1 })
+        wx.showLoading({ title: '加载更多...', mask: true });
+      }
+    } else {
+      if (that.data.on_passRefersh) {
+        that._request({ pagenum: that.data.on_passPagenum + 1 });
+        wx.showLoading({ title: '加载更多...', mask: true });
+      }
+    }
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 })
--- mine/myAssociation/associationInfo/associationInfo.js
+++ mine/myAssociation/associationInfo/associationInfo.js
@@ -56,6 +56,11 @@ Page({
   goTo_notice(){
     app.globalData.goToPage("../noticeList/noticeList")
   },
+
+  //申请入社列表
+  goTo_applyList(){
+    app.globalData.goToPage("../applyList/applyList")
+  },
   
   //面板提示信息
   _panelReauest(clubid){
